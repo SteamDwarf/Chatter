5ce7645eb2cde5de30f3929adb2ef2848d1cc7b5
Dmitriy <schajckodmitrij@gmail.com>
2024-01-25 13:50:19 +0300

    fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -44,8 +44,8 @@ export enum SocketEvents {
 app.use(cors());
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', (req, res) => res.sendFile(path.join(__dirname+'client/build/index.html')));
+    //app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', (req, res) => res.json({message: "Chatter work!"}));
 }
 
 server.listen(PORT, () => {
